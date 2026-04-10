106b6f2526323ebdf81957e417e22a84babb5885
Usu171 <usu171@proton.me>
2026-04-10 17:01:06 +0800

    feat: footer time setting move to config

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,12 @@ export default {
   USERNAME: "Usu171",
   SITE_TITLE: "Usu171's Blog",
   SITE_URL: "https://usu171.uk",
-  TIME_ZONE: "UTC",
   USER_IMG: "/img/avatar.webp",
   favicon: "/img/favicon.webp",
   USER_EMIAL: "usu171@usu171.uk",
+  TIME_ZONE: "UTC",
+  BLOG_START_DATE: "2023-05-07",
+  COPYRIGHT_START_YEAR: 2023,
   Links: [
     {
       name: "GitHub",
